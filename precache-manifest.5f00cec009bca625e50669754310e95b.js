21b480f89f3d95812908d1c344228ac98baa8801
Janine <janine.newton@mail.utoronto.ca>
2020-10-03 16:36:51 -0400

    Updates

---
+++ precache-manifest.5f00cec009bca625e50669754310e95b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c041e316e34c1c659dc35ff76d289099",
+    "url": "/planno2/index.html"
+  },
+  {
+    "revision": "d124e809759cf361b654",
+    "url": "/planno2/static/css/2.0012cb62.chunk.css"
+  },
+  {
+    "revision": "510f06c8b06f7f5f13b7",
+    "url": "/planno2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d124e809759cf361b654",
+    "url": "/planno2/static/js/2.a0642cde.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/planno2/static/js/2.a0642cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510f06c8b06f7f5f13b7",
+    "url": "/planno2/static/js/main.6ab8633a.chunk.js"
+  },
+  {
+    "revision": "87cff6bb9dc5f74c835c",
+    "url": "/planno2/static/js/runtime-main.a6406b88.js"
+  },
+  {
+    "revision": "9af315a4756134eaf0224592c7458664",
+    "url": "/planno2/static/media/pencil.9af315a4.svg"
+  }
+]);
